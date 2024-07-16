6cc773be3c80bcd73e4b1d27e9899dff39847db0
daeun088 <kde2329@naver.com>
2024-07-16 18:59:18 +0900

    Feat : Route 설정

--- green-field/src/App.js
+++ green-field/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import QnA from './pages/QnA';
+import Together from './pages/Together';
+import Community from './pages/Community';
+import Login from './pages/Login';
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <Router>
-      <Header>
-      <Route exact path = "/" component = {Home}/>
-      </Header>
+      <Header />
+      <Routes>
+      <Route path="/" element={<Home />} />
+        <Route path="/qna" element={<QnA />} />
+        <Route path="/together" element={<Together />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/login" element={<Login />} />
+        </Routes>
         
     </Router>
   );
---
+++ green-field/src/pages/QnA.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const QnA = () => {
+  return (
+    <div>
+      <h1>Qna Page</h1>
+      <h1>Qna Page</h1>
+      <h1>Qna Page</h1><h1>Qna Page</h1>
+    </div>
+  );
+}
+
+export default QnA;
